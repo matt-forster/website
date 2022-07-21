2bb678345bc4dab3eb934e291b9bd76ed2dc10c1
Matt Forster <hey@mattforster.ca>
2022-07-20 23:03:34 -0600

    feat: meta

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
 import { Main } from './components';
+import { MetaProvider, Title, Meta } from 'solid-meta';
 
-export default () => <Main />
+export default () => (
+  <MetaProvider>
+    <Meta charset="utf-8" />
+    <Meta name="viewport" content="width=device-width, initial-scale=1" />
+    <Meta name="theme-color" content="#000000" />
+    <link rel="shortcut icon" type="image/ico" href="/src/assets/favicon.ico" />
+    <Title>Matt Forster</Title>
+    <Main />
+  </MetaProvider>
+)
